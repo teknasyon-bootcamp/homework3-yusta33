c346af3f7e07e2d4387b65c2416ab4535ba8d803
yusta33 <66492360+yusta33@users.noreply.github.com>
2021-09-04 21:54:18 +0300

    Update form.php

--- form.php
+++ form.php
@@ -24,3 +24,54 @@
  * 
  * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
  */
+class Form
+{   //Sinif ozellikleri tanimlamalari//
+    private string $action;
+    private string $method;
+    public array $fields;
+
+    private function __construct(string $action,string $method)
+    {//Action ve method degerleri atamasi.//
+        $this->action = $action;
+        $this->method = $method;
+    }
+    //Istenen static formlari//
+    public static function createForm(string $action,string $method): Form
+    {
+        return new static($action,$method);
+    }
+    public static function createGetForm(string $action): Form
+    {
+        return new static($action,"GET");
+    }
+    public static function createPostForm(string $action): Form
+    {
+        return new static($action,"POST");
+    }
+    public function addField(string $label,string $name,?string $defaultValue = null): void
+    {//Degerler diziye eklendi//
+        $this->fields[$name]=
+        [
+            "label"=> $label,
+            "name"=> $name,
+            "defaultValue"=>$defaultValue 
+        ];
+    }
+    public function setMethod(string $method): void
+    {   //Method set edildi//
+        $this->method=$method;
+    }
+    public function render(): void
+    {
+        //Html ciktisi//
+        echo "<form method='$this->method' action='$this->action'>";
+      
+        foreach ($this->fields as $field) 
+        {// Dizi oldugu icin foreach dongusu kullanildi//
+            echo "<label for='$field[name]'>$field[label] </label>" . PHP_EOL;
+            echo "<input type='text' name='$field[name]' value='$field[defaultValue]'/>";
+        }
+        echo "<button type='submit'>Gönder</button>";
+        echo "</form>";
+    }
+}
